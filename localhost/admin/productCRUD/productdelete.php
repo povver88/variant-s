0e0ebdc7d9d43c1ca136f1087c6c3c8d18e56bb4
aquamarissa <vremapriklucenij16@gmail.com>
2019-05-15 21:38:53 +0300

    fixdelete

--- localhost/admin/productCRUD/productdelete.php
+++ localhost/admin/productCRUD/productdelete.php
@@ -3,12 +3,15 @@ session_start();
 $db = mysqli_connect('localhost', 'root', '', 'users');
 if (isset($_POST['productName'])) {
     $productName = e($_POST['productName']);
-    $query = mysqli_query($db,"DELETE From products WHERE name='$productName'");
-    $photo = $_SESSION['editproduct']['Photo'];
-    $aphoto = $_SESSION['editproduct']['ArticlePhoto'];
+    $query = mysqli_query($db,"SELECT * FROM products WHERE name='$productName'");
+    while ($row = mysqli_fetch_assoc($query)) {
+        $photo = $row['Photo'];
+        $aphoto = $row['ArticlePhoto'];
+    }
     unlink("../../photos/product/{$photo}.jpg");
     unlink("../../photos/article/{$aphoto}.jpg");
-    header('location: productslist.php');
+    $query = mysqli_query($db,"DELETE From products WHERE name='$productName'");
+    header('location: ../productslist.php');
 }
 
 function e($val){
